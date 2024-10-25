fce776b926c34bdb7bacfb70b604e450bd4bd664
Lucas Falla Urtiaga <falla.urluc24@triana.salesianos.edu>
2024-10-25 12:30:25 +0200

    Detalles pokémon sin descripción y categoría acabados.

--- Pokemon-APP/src/app/components/pokemon-card/pokemon-card.component.ts
+++ Pokemon-APP/src/app/components/pokemon-card/pokemon-card.component.ts
@@ -20,85 +20,62 @@ export class PokemonCardComponent implements OnInit{
     });
   }
 
-  cambiarColorSegunTipo(nombreTipo1: string): void{
-
-    switch(nombreTipo1){
-      case "normal":
-        document.querySelector('.tipoDominante')!.setAttribute('style', 'background-color: rgba(170,166,127,.4) !important');
-        break;
-
-      /*case "fighting":
-          $(`.tipoDominante${pokemonId}`).attr("style", "background-color: #C12239");
-          break;
-
-      case "flying":
-          $(`.tipoDominante${pokemonId}`).attr("style", "background-color: #A891EC");
-          break;
-
-      case "ground":
-          $(`.tipoDominante${pokemonId}`).attr("style", "background-color: #DEC16B");
-          break;
-
-      case "poison":
-          $(`.tipoDominante${pokemonId}`).attr("style", "background-color: #A43E9E");
-          break;
-      
-      case "rock":
-          $(`.tipoDominante${pokemonId}`).attr("style", "background-color: #B69E31");
-          break; 
-
-      case "bug":
-          $(`.tipoDominante${pokemonId}`).attr("style", "background-color: #A7B723");
-          break; 
-
-      case "ghost":
-          $(`.tipoDominante${pokemonId}`).attr("style", "background-color: #70559B");
-          break; 
-
-      case "steel":
-          $(`.tipoDominante${pokemonId}`).attr("style", "background-color: #B7B9D0");
-          break;*/
-
-      case "fire":
-          document.querySelector('.tipoDominante')!.setAttribute('style', 'background-color: #F57D31 !important');
-          break;
-      
-      /*case "water":
-          $(`.tipoDominante${pokemonId}`).attr("style", "background-color: #6493EB");
-          break;*/
-
-      case "grass":
-          document.querySelector('.tipoDominante')!.setAttribute('style', 'background-color: rgba(116,203,72,.4) !important');
-          break;
-
-      /*case "electric":
-          $(`.tipoDominante${pokemonId}`).attr("style", "background-color: #F9CF30");
-          break;
-
-      case "psychic":
-          $(`.tipoDominante${pokemonId}`).attr("style", "background-color: #FB5584");
-          break;
-
-      case "ice":
-          $(`.tipoDominante${pokemonId}`).attr("style", "background-color: #9AD6DF");
-          break;
-
-      case "dragon":
-          $(`.tipoDominante${pokemonId}`).attr("style", "background-color: #7037FF");
-          break;
-
-      case "dark":
-          $(`.tipoDominante${pokemonId}`).attr("style", "background-color: #75574C");
-          break;
-
-      case "fairy":
-          $(`.tipoDominante${pokemonId}`).attr("style", "background-color: #E69EAC");
-          break;*/
-      
-      default:
-          document.querySelector('.tipoDominante')!.setAttribute('style', 'background-color: rgba(116,203,72,.4) !important');
+  cambiarColorOpacidadSegunTipo(nombreTipo1: string): string{
+    interface Colores {
+        [index: string]: string;
     }
+
+    let coloresTipo = {
+        "normal": "rgba(170,166,127, .4)",
+        "fighting": "rgb(193, 34, 57, .4)",
+        "flying": "rgb(168, 145, 236, .4)",
+        "ground": "rgb(222, 193, 107, .4)",
+        "poison": "rgb(164, 62, 158, .4)",
+        "rock": "rgb(182, 158, 49, .4)",
+        "bug": "rgb(167, 183, 35, .4)",
+        "ghost": "rgb(112, 85, 155, .4)",
+        "steel": "rgb(183, 185, 208, .4)",
+        "fire": "rgb(245, 125, 49, .4)",
+        "water": "rgb(100, 147, 235, .4)",
+        "grass": "rgba(116,203,72,.4)",
+        "electric": "rgb(249, 207, 48, .4)",
+        "physic": "rgb(251, 85, 132, .4)",
+        "ice": "rgb(154, 214, 223, .4)",
+        "dragon": "rgb(112, 55, 255, .4)",
+        "dark": "rgb(117, 87, 76, .4)",
+        "fairy": "rgb(230, 158, 172, .4)"
+    } as Colores
+
+    return `background-color: ${coloresTipo[nombreTipo1]}`
+  }
+
+  cambiarColorSegunTipo(nombreTipo: string): string{
+    interface Colores {
+        [index: string]: string;
+    }
+
+    let coloresTipo = {
+        "normal": "rgba(170,166,127)",
+        "fighting": "rgb(193, 34, 57)",
+        "flying": "rgb(168, 145, 236)",
+        "ground": "rgb(222, 193, 107)",
+        "poison": "rgb(164, 62, 158)",
+        "rock": "rgb(182, 158, 49)",
+        "bug": "rgb(167, 183, 35)",
+        "ghost": "rgb(112, 85, 155)",
+        "steel": "rgb(183, 185, 208)",
+        "fire": "rgb(245, 125, 49)",
+        "water": "rgb(100, 147, 235)",
+        "grass": "rgba(116,203,7)",
+        "electric": "rgb(249, 207, 48)",
+        "physic": "rgb(251, 85, 132)",
+        "ice": "rgb(154, 214, 223)",
+        "dragon": "rgb(112, 55, 255)",
+        "dark": "rgb(117, 87, 76)",
+        "fairy": "rgb(230, 158, 172)"
+    } as Colores
+
+    return `background-color: ${coloresTipo[nombreTipo]}`
   }
   
-
 }
--- Pokemon-APP/src/app/components/pokemon-detail/pokemon-detail.component.ts
+++ Pokemon-APP/src/app/components/pokemon-detail/pokemon-detail.component.ts
@@ -2,7 +2,6 @@ import { Component, OnInit } from '@angular/core';
 import { PokeomonDetailResponse } from '../../interfaces/pokemon-detail.interfaces';
 import { ActivatedRoute } from '@angular/router';
 import { PokemonService } from '../../services/pokemon.service';
-import { PokemonDesCatResponse } from '../../interfaces/pokemon-des-cat.interfaces';
 
 @Component({
   selector: 'app-pokemon-detail',
@@ -25,7 +24,64 @@ export class PokemonDetailComponent implements OnInit{
     this.pokemonService.getPokemon(parseInt(this.pokemonId!)).subscribe(respuesta => {
       this.pokemon = respuesta;
     });
+  }
 
+  cambiarColorSegunTipo(nombreTipo: string): string{
+    interface Colores {
+        [index: string]: string;
+    }
+
+    let coloresTipo = {
+        "normal": "rgba(170,166,127)",
+        "fighting": "rgb(193, 34, 57)",
+        "flying": "rgb(168, 145, 236)",
+        "ground": "rgb(222, 193, 107)",
+        "poison": "rgb(164, 62, 158)",
+        "rock": "rgb(182, 158, 49)",
+        "bug": "rgb(167, 183, 35)",
+        "ghost": "rgb(112, 85, 155)",
+        "steel": "rgb(183, 185, 208)",
+        "fire": "rgb(245, 125, 49)",
+        "water": "rgb(100, 147, 235)",
+        "grass": "rgba(116,203,7)",
+        "electric": "rgb(249, 207, 48)",
+        "physic": "rgb(251, 85, 132)",
+        "ice": "rgb(154, 214, 223)",
+        "dragon": "rgb(112, 55, 255)",
+        "dark": "rgb(117, 87, 76)",
+        "fairy": "rgb(230, 158, 172)"
+    } as Colores
+
+    return `background-color: ${coloresTipo[nombreTipo]}`
+  }
+
+  cambiarColorTextoSegunTipo(nombreTipo: string): string{
+    interface Colores {
+        [index: string]: string;
+    }
+
+    let coloresTipo = {
+        "normal": "rgba(170,166,127)",
+        "fighting": "rgb(193, 34, 57)",
+        "flying": "rgb(168, 145, 236)",
+        "ground": "rgb(222, 193, 107)",
+        "poison": "rgb(164, 62, 158)",
+        "rock": "rgb(182, 158, 49)",
+        "bug": "rgb(167, 183, 35)",
+        "ghost": "rgb(112, 85, 155)",
+        "steel": "rgb(183, 185, 208)",
+        "fire": "rgb(245, 125, 49)",
+        "water": "rgb(100, 147, 235)",
+        "grass": "rgba(116,203,7)",
+        "electric": "rgb(249, 207, 48)",
+        "physic": "rgb(251, 85, 132)",
+        "ice": "rgb(154, 214, 223)",
+        "dragon": "rgb(112, 55, 255)",
+        "dark": "rgb(117, 87, 76)",
+        "fairy": "rgb(230, 158, 172)"
+    } as Colores
+
+    return `color: ${coloresTipo[nombreTipo]}`
   }
 
 }
